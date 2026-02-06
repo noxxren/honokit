36046f56bcfb42010a4077851ea7dd597e859145
krukowski <rafal.krukowski@antologic.com>
2026-02-06 12:01:47 +0100

    feat: HONOKIT-0002

--- apps/client/src/redux/slices/authSlice.ts
+++ apps/client/src/redux/slices/authSlice.ts
@@ -1,7 +1,7 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 import type { User } from '@honokit/shared/types'
 
-interface AuthState {
+export interface AuthState {
   user: User | null
   token: string | null
   isAuthenticated: boolean
